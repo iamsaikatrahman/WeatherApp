0f7de13b3e658ec50fcb41423fcf3bff2e92f8bb
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-06-08 09:05:18 +0600

    Android weather app fetch json data using volley

---
+++ app/src/main/java/com/saikat/weatherapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.saikat.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_datainput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+
+        //click listeners for each button.
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "City ID is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Weather id is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Weather name is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
