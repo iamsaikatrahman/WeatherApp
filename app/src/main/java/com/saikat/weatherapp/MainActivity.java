e830bf40a404f28f1d48251f2f7e4b59a594635a
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-06-09 08:59:19 +0600

    volley implement

--- app/src/main/java/com/saikat/weatherapp/MainActivity.java
+++ app/src/main/java/com/saikat/weatherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.saikat.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,24 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
     EditText et_dataInput;
-    ListView lv_weatherReport;
+    RecyclerView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +45,51 @@ public class MainActivity extends AppCompatActivity {
         btn_cityID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "City ID is Clicked", Toast.LENGTH_SHORT).show();
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=7cd799f14c810c9b2181243beeeaac66&units=metric";
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String temperature = "";
+                        try {
+                            JSONObject tempInfo = response.getJSONObject("main");
+                            temperature = tempInfo.getString("temp");
+                            JSONObject cityInfo = response.getJSONObject("id");
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(MainActivity.this, "Today Temperature " + temperature, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Something wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(request);
+
+//                // Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // Display the first 500 characters of the response string.
+//                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(MainActivity.this, "Error occured", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+
+                //Toast.makeText(MainActivity.this, "City ID is Clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -48,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Weather name is clicked", Toast.LENGTH_SHORT).show();
             }
         });
-
 
 
     }
